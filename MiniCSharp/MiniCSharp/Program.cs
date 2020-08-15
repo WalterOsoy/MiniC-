1f3c37dd035354029595eb04fca6563ad37948c5
Andres Pineda <apiro99@outlook.com>
2020-08-15 12:50:00 -0600

    testing vs code

--- MiniCSharp/MiniCSharp/Program.cs
+++ MiniCSharp/MiniCSharp/Program.cs
@@ -2,11 +2,11 @@
 
 namespace MiniCSharp
 {
-    class Program
+  class Program
+  {
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
-        }
+      Console.WriteLine("Hello World from vs code!");
     }
+  }
 }
