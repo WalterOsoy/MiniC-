c55ebc052cbdb69b869219e8e0b3cc276d6c34e6
WalterOsoy <31348535+WalterOsoy@users.noreply.github.com>
2020-08-15 12:45:06 -0600

    Proyecto base

---
+++ MiniCSharp/MiniCSharp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MiniCSharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
