7339ea3da08f5aab98d36aaf38dde80a611c0c0c
WalterOsoy <31348535+WalterOsoy@users.noreply.github.com>
2020-08-15 23:10:00 -0600

    Create LexicalAnalyzer.cs

---
+++ MiniCSharp/MiniCSharp/LexicalAnalyzer.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace MiniCSharp
+{
+    class LexicalAnalyzer
+    {
+        List<string> reserved = new List<string>()
+        { "void", "int", "double", "bool", "string",
+          "class", "const", "interface", "null" ,"this" ,"for",
+          "while", "foreach", "if", "else" ,"return" ,"breack",
+          "New", "NewArray", "Console", "WriteLine" };
+        Regex id = new Regex(@"[a-zA-Z]+[a-zA-z|_|0-9]*");
+        Regex ndouble = new Regex(@"^[0-9]+[.]?[0-9]*$");
+        //^[0-9]+[.]?[0-9]*(E[+]|E)[0-9]+$
+        Regex heza = new Regex(@"([0-9]+[.]?[0-9](e|e[+]|E[+]|E)?[0-9])");
+        public void ToAnalyze()
+        {
+            string s = "sapo sapo_9 9sapo";
+            var b = id.Match(s);
+            var c = id.Matches(s);
+
+            string f = ".12 12.5 12. 12.E2 12.e+2";
+            var g = heza.Matches(f);
+        }
+    }
+}
