caaa41e8a9324e60fec55e02796e56cb9c3ca3e3
WalterOsoy <walter.osoy@hotmail.com>
2020-10-22 17:19:09 -0600

    estructura basica del parseo

--- MiniCSharp/MiniCSharp/Clases/TableParser.cs
+++ MiniCSharp/MiniCSharp/Clases/TableParser.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.Windows.Forms;
 using System.Text.RegularExpressions;
 using DataStructures;
+using System.Runtime.InteropServices;
 
 namespace Clases
 {
@@ -11,21 +12,69 @@ namespace Clases
     class TableParser
     {
         List<Token> tokensList;
+        Stack<int> stack;
+        Stack<Token> symbol;
+        string action;
         private string path;
         private Dictionary<int, Dictionary<string, string>> table;
         public TableParser(string path, ref List<Token> tokensList)
-        {
-            this.tokensList = new List<Token>(tokensList);
+        {            
             this.path = path;
             table = new Dictionary<int, Dictionary<string, string>>();
             readTable();
+            this.tokensList = new List<Token>(tokensList);
+            stack = new Stack<int>();
+            symbol = new Stack<Token>();
+            action = "";
         }
 
         public Dictionary<int, Dictionary<string, string>> getTable()
         {
             return table;
         }
+        public void parse()
+        {
+            bool end = false;
+            bool complete = false;
+            do
+            {
+                int fila = stack.Peek();
+                Token entrada = tokensList[0];
+                action = table[fila][entrada.Value];
+                switch (action[0])
+                {
+                    case 's'://desplazamiento
+                        Displacement();
+                        break;
+                    case 'r'://reduccion 
+                        Reduction();
+                        break;
+                    case 'a'://Aceptar
+                        complete = true;
+                        end = true;
+                        break;
+                    case 'e'://error
+                        complete = false;
+                        end = true;
+                        break;
+                    default://ir a 
+                        GoTo();
+                        break;
+                }
+            } while (end != true);
+        }
+        private void Displacement()
+        {   
+            
+        }
+        private void Reduction()
+        {
 
+        }
+        private void GoTo()
+        {
+
+        }
         private void readTable()
         {
             using (StreamReader sr = new StreamReader(path))
@@ -41,7 +90,6 @@ namespace Clases
                 }
             }
         }
-
         private Dictionary<string, string> readHeaders(StreamReader sr)
         {
             string[] headers = Regex.Split(sr.ReadLine(), ",(?=(?:[^\"]*\"[^\"]*\")*(?![^\"]*\"))", RegexOptions.IgnorePatternWhitespace);
@@ -49,7 +97,6 @@ namespace Clases
             foreach (string header in headers) rtrnDict.Add(CheckValue(header), "");
             return rtrnDict;
         }
-
         private Dictionary<string, string> readContent(string Line, Dictionary<string, string> template)
         {
             string[] lineValues = Line.Split(',');
@@ -65,12 +112,12 @@ namespace Clases
 
             return template;
         }
-
         private string CheckValue(string value)
         {
             return (Regex.IsMatch(value, @"([\S]*\,[\S]*)+"))
               ? value.Trim('"')
               : value;
         }
+        
     }
 }
