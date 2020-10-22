18da13ce0927d78095f8fc98060c0c3fddb467a0
WalterOsoy <walter.osoy@hotmail.com>
2020-10-22 13:01:20 -0600

    lista de tokens

---
+++ MiniCSharp/MiniCSharp/DataStructures/Token.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataStructures
+{
+    class Token
+    {
+        public string Value { get; set; }
+        public string type { get; set; }
+    }
+}
