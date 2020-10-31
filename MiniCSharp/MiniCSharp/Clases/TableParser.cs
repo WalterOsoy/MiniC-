2e3393eadad278825f8d62b183f45af43b2e107a
WalterOsoy <walter.osoy@hotmail.com>
2020-10-31 06:12:05 -0600

    commit final

--- MiniCSharp/MiniCSharp/Clases/TableParser.cs
+++ MiniCSharp/MiniCSharp/Clases/TableParser.cs
@@ -1,16 +1,16 @@
+using System.Diagnostics.SymbolStore;
+using System.Collections;
 using System;
-using System.IO;
 using System.Collections.Generic;
-using System.Windows.Forms;
-using System.Text.RegularExpressions;
-using DataStructures;
+using System.IO;
 using System.Runtime.InteropServices;
+using System.Text.RegularExpressions;
+using System.Windows.Forms;
+using DataStructures;
 
-namespace Clases
-{
+namespace Clases {
 
-    class TableParser
-    {
+    class TableParser {
         List<Token> tokensList;
         Stack<int> stack;
         Stack<string> symbol;
@@ -18,100 +18,90 @@ namespace Clases
         Dictionary<int, Dictionary<string, string>> table;
         Dictionary<int, Dictionary<string, List<string>>> grammar;
         string getnumbre = @"[0-9]+";
-        public TableParser(ref List<Token> tokensList)
-        {            
+        public TableParser (ref List<Token> tokensList) {
             this.tokensList = tokensList;
-            table = new Dictionary<int, Dictionary<string, string>>();            
-            grammar = new Dictionary<int, Dictionary<string, List<string>>>();
-            new DataLoader(ref table, ref grammar);
-            stack = new Stack<int>();
-            symbol = new Stack<string>();
-            stack.Push(0);
+            table = new Dictionary<int, Dictionary<string, string>> ();
+            grammar = new Dictionary<int, Dictionary<string, List<string>>> ();
+            new DataLoader (ref table, ref grammar);
+            stack = new Stack<int> ();
+            symbol = new Stack<string> ();
+            stack.Push (0);
             action = "";
-            tokensList.Add(new Token{type="$",Value="$"});
+            tokensList.Add (new Token { type = "$", Value = "$" });
         }
-        public void parse()
-        {
+        public void parse () {
             bool end = false;
             bool okey = false;
             int fila;
-            Token entrada ;
-            do
-            {
-                fila = stack.Peek();
+            Token entrada;
+            do {
+                fila = stack.Peek ();
                 entrada = tokensList[0];
                 action = table[fila][entrada.type];
-                switch (action[0])
-                {
-                    case 's'://desplazamiento                        
-                        Displacement(false);
+                switch (action[0]) {
+                    case 's': //desplazamiento                        
+                        Displacement (false);
                         break;
-                    case 'r'://reduccion 
-                        Reduction();
+                    case 'r': //reduccion 
+                        Reduction ();
                         break;
-                    case 'a'://Aceptar
+                    case 'a': //Aceptar
                         okey = true;
                         end = true;
                         break;
-                    case 'e'://error 
-                        action = table[stack.Peek()]["Ɛ"];
-                        switch (action[0])
-                        {
-                            case 's':                                
-                                Displacement(true);
+                    case 'e': //error 
+                        action = table[stack.Peek ()]["Ɛ"];
+                        switch (action[0]) {
+                            case 's':
+                                Displacement (true);
                                 break;
-                            case 'r':                                
-                                Reduction();
+                            case 'r':
+                                Reduction ();
                                 break;
                             case 'e':
-                            end = true;
-                            okey= false;
-                                break;                                                 
+                                end = true;
+                                okey = false;
+                                break;
                         }
-                        break;                    
+                        break;
                 }
             } while (end != true);
-            if(okey){//all okey
-                Console.WriteLine("Parseo exitoso");
-            }else{//error
-                Console.WriteLine("Error en el parse en: "+ tokensList[0].Value +" - > "+ fila+", "+entrada.type+", "+entrada.Value);
+            if (okey) { //all okey
+                Console.WriteLine ("Parseo exitoso");
+            } else { //error
+                Console.WriteLine ("Error en el parse en: " + tokensList[0].Value + " - > " + fila + ", " + entrada.type + ", " + entrada.Value);
             }
         }
-        private void Displacement(bool epsilon)
-        {      
-            stack.Push(Convert.ToInt32(Regex.Match(action, getnumbre).ToString()));                    
-            if(!epsilon){            
-                symbol.Push(tokensList[0].type);
-                tokensList.RemoveAt(0);
-            }else{
-                symbol.Push("Ɛ");
-            }            
+        private void Displacement (bool epsilon) {            
+            stack.Push (Convert.ToInt32 (Regex.Match (action, getnumbre).ToString ()));
+            if (!epsilon) {
+                symbol.Push (tokensList[0].type);
+                tokensList.RemoveAt (0);
+            } else {
+                symbol.Push ("Ɛ");
+            }
         }
-        private void Reduction( )
-        {
-            int num = Convert.ToInt32(Regex.Match(action, getnumbre).ToString());            
-            List<string> production = new List<string>(grammar[num].Keys);
-            List<string> elemens = new List<string>(grammar[num][production[0]]);
+        private void Reduction () {
+            int num = Convert.ToInt32 (Regex.Match (action, getnumbre).ToString ());
+            List<string> production = new List<string> (grammar[num].Keys);
+            List<string> elemens = new List<string> (grammar[num][production[0]]);
             //quita la cantidad de elementos en la pila segun la cantidad de elementos en la produccion 
-            for (int i = 0; i < elemens.Count; i++)
-            {
-                stack.Pop();
+            for (int i = 0; i < elemens.Count; i++) {
+                stack.Pop ();
             }
             //quita la cantidad de elementos en los simbolos segun la cantidad de elementos en la produccion 
-            for (int i = 0; i < elemens.Count; i++)
-            {
-                symbol.Pop();
+            for (int i = 0; i < elemens.Count; i++) {
+                symbol.Pop ();
             }
             //inserta la produccion 
-            symbol.Push(production[0]);
-            GoTo();
+            symbol.Push (production[0]);
+            GoTo ();
         }
-        private void GoTo()
-        {
-            int fila = stack.Peek();
-            string entrada = symbol.Peek();
+        private void GoTo () {
+            int fila = stack.Peek ();
+            string entrada = symbol.Peek ();
             action = table[fila][entrada];
-            stack.Push(Convert.ToInt32(action));
-        }     
+            stack.Push (Convert.ToInt32 (action));
+        }
     }
 }
